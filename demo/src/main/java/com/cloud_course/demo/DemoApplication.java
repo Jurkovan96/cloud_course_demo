5e5fc0f909896415772ad6ed320b513323c6f8b2
Rares Vasiliu <jurkovan.rv@gmail.com>
2021-12-30 22:55:25 +0200

    Profile setup

---
+++ demo/src/main/java/com/cloud_course/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.cloud_course.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(DemoApplication.class);
+        springApplication.setAdditionalProfiles("azure");
+        springApplication.run();
+    }
+}
---
+++ demo/src/test/java/com/cloud_course/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_course.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
