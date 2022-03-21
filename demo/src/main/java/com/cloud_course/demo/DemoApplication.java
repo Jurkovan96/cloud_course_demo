0476d00c82bb6b371571c3727aae1d4795f04e82
Rares Vasiliu <jurkovan.rv@gmail.com>
2022-03-21 18:28:36 +0200

    This is a new commit on this branch!

--- demo/src/main/java/com/cloud_course/demo/DemoApplication.java
+++ demo/src/main/java/com/cloud_course/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("this is a custom log!");
+        System.out.println("this is a second custom log!");
+
         SpringApplication springApplication = new SpringApplication(DemoApplication.class);
         springApplication.setAdditionalProfiles("amazon");
         springApplication.run();
